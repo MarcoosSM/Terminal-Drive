bab6dbbab911ebc6ea66568a4e49377d604aa37c
Marcos <44418083+MarcoosSM@users.noreply.github.com>
2018-10-29 19:22:03 +0100

    Escena inicial y animacion idle
    Hay algún bug con las animaciones

---
+++ Terminal Drive/Assets/Player/Control/PlayerControlMouse.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlMouse : MonoBehaviour {
+
+	[SerializeField] GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		Vector3 springPlace = gameObject.GetComponent<Camera>().ScreenToWorldPoint
+            (new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+ 		
+		  target.transform.position = new Vector3(springPlace.x, springPlace.y, 0);
+            
+        }
+		
+	
+}
