3fa49ed6ab095c23bbac08d9bcaa6987b6ceb2db
Alexander <40186295+malyga@users.noreply.github.com>
2018-10-31 15:03:44 +0100

    Cleanup del script de movimiento del jugador

--- Terminal Drive/Assets/Player/Control/PlayerControl.cs
+++ Terminal Drive/Assets/Player/Control/PlayerControl.cs
@@ -2,67 +2,72 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
 
-    public float speed;
-    public Vector2 jumpHeight = new Vector2(0, 10);
-    Rigidbody2D RB2d;
+    private Rigidbody2D rb2d;
     private Animator animator;
-    private SpriteRenderer sr;
-    private bool isGrounded;
 
-	// Use this for initialization
-	void Start () {
-		RB2d = GetComponent<Rigidbody2D>();
-        speed = 5;
+    // Variables públicas
+    public float moveSpeed = 5;
+    public Vector2 jumpHeight = new Vector2(0, 10);
+    public float rangeStartMovement = 0.3f;
+    // FIN variables públicas
+
+    // Variables de control del jugador
+    private float hAxis;
+    private bool jumpKeyPressed;
+    // FIN variables de control del jugador
+
+    void Awake() {
+        rb2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        sr = GetComponent<SpriteRenderer>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        checkIsJumping();
-        if (Input.GetButtonDown("Jump"))
-        {
-            Debug.Log("JUMP!");
-            if (!animator.GetBool("isJumping"))
-            {
-                // Si no esta saltando, añade la fuerza del salto
-                RB2d.AddForce(jumpHeight, ForceMode2D.Impulse);
+    }
+    void Start() {
+        
+    }
+
+    void Update() {
+        updateKeyInput();
+
+        updateIsJumping();
+        if(jumpKeyPressed) {
+            if(! animator.GetBool("isJumping")) {
+                rb2d.AddForce(jumpHeight, ForceMode2D.Impulse);
                 animator.SetBool("isJumping", true);
             }
-        }
-    }
-
-	void FixedUpdate()
-    {
-        move();
-    }
-
-    private void move()
-    {
-        float h = Input.GetAxis("Horizontal");
-
-        checkFlip(h);
-
-        float absH = Mathf.Abs(h);
-
-        if (absH > 0.3) // Solo se mueve si la fuerza h es mayor que cierto número
-        {
-            animator.SetFloat("Speed", absH * speed);
-            RB2d.velocity = new Vector2(speed * h, RB2d.velocity.y);
-        }
-        else 
-        {
-            // Stop al jugador y a la animación
-            animator.SetFloat("Speed", 0);
-            RB2d.velocity = new Vector2(0, RB2d.velocity.y);
         }
         
     }
 
-    private void checkFlip(float h)
-    {
-        if(h != 0) {
-            if(h > 0) {
+    void FixedUpdate() {
+        move();
+    }
+
+    private void updateKeyInput() {
+        // Actualiza las variables de control del jugador
+        hAxis = Input.GetAxis("Horizontal");
+        jumpKeyPressed = Input.GetButtonDown("Jump");
+       
+    }
+
+    private void move() {
+        // Mueve al jugador, dependiendo de los valores de las variables de control
+        updateOrientation();
+
+        float absHAxis = Mathf.Abs(hAxis);
+        if(absHAxis > rangeStartMovement) {
+            animator.SetFloat("Speed", absHAxis * moveSpeed);
+            rb2d.velocity = new Vector2(moveSpeed * hAxis, rb2d.velocity.y);
+        } else {
+            // Si no hay suficiente movimiento en el hAxis, para al jugador
+            animator.SetFloat("Speed", 0);
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+        }
+
+    }
+    
+    private void updateOrientation() {
+        // Cambia la dirección a la que mira el jugador dependiendo de la entrada por teclado
+        if(hAxis != 0) {
+            if(hAxis > 0) {
                 animator.SetBool("isRight", true);
             } else {
                 animator.SetBool("isRight", false);
@@ -70,18 +75,10 @@ public class PlayerControl : MonoBehaviour {
         }
     }
 
-    private void checkIsJumping()
-    {
+    private void updateIsJumping() {
         // Comprueba la velocidad del eje y del personaje y si es 0 significa que no esta saltando
-        if (RB2d.velocity.y == 0)
-        {
+        if(rb2d.velocity.y == 0) {
             animator.SetBool("isJumping", false);
         }
-        /**
-         * Precaución: Puede darse que la Y sea 0 cuando el personaje esta 
-         * en la parte superior del arco del salto, cuando ha dejado de subir pero todavia no esta bajando.
-         * En este caso sería posible que el jugador hiciese un segundo salto.
-         **/
     }
-    
 }
