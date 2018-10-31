485f97a8c87a5b2b76848df0615b7f1576b740e2
Nismal <ismaelperez1299@gmail.com>
2018-10-31 11:53:31 +0100

    Añadido el movimiento del brazo teniendo el cuenta la orientacion de "player"
    - Tambien he cambiado el order in layer de player a 1 para que el renderizado del brazo se hiciese bien.

--- Terminal Drive/Assets/Player/Control/HandPointerControl.cs
+++ Terminal Drive/Assets/Player/Control/HandPointerControl.cs
@@ -5,33 +5,46 @@ using UnityEngine;
 public class HandPointerControl : MonoBehaviour {
 
 	[SerializeField] GameObject target;
-	[SerializeField]GameObject hand;
+	GameObject hand;
 	Camera mainCamera;
+
+	private Animator animator;
+
 	// Use this for initialization
 	void Start () {
+		animator = GetComponent<Animator>();
 		mainCamera = Camera.main;
-		Debug.Log(mainCamera);
 
+		//Se busca la mano el jugador
 		foreach (Transform child in transform){
 			if(child.name=="Hand"){
 				
 				hand = child.gameObject;
-				Debug.Log(hand);
 			}
 		}
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-
+		//Codigo para mover el cursor
 		Vector3 pointer = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
- 		
 		target.transform.position = new Vector3(pointer.x, pointer.y, 0);
 
+		//Cursor para mover el brazo
 		var dir = transform.position - target.transform.position;
  		var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
  		hand.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
+		//Para ver si el brazo se encuetra detras o delante del jugador
+
+		if(animator.GetBool("isRight")){
+
+			hand.GetComponent<SpriteRenderer>().sortingOrder=2;
+
+		}else{
+
+			hand.GetComponent<SpriteRenderer>().sortingOrder=1;
+		}
 
     }
 
