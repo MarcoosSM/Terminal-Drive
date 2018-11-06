6b762844464e6b434add30af41b4f7810cf6e5bc
Nismal <40026531+Nismal@users.noreply.github.com>
2018-11-06 15:43:10 +0100

    Separado el script que controla el pointer y la mano

---
+++ Terminal Drive/Assets/Resources/Player/Control/HandControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandControl : MonoBehaviour {
+
+	GameObject Cursor ;
+	GameObject weapon;
+	private Animator animator;
+
+	
+	
+	// Use this for initialization
+	void Start () {
+		weapon = transform.GetChild(0).gameObject;
+		animator = GetComponent<Animator>();
+		Cursor = GameObject.FindGameObjectWithTag("Pointer");
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+				//Cursor para mover el brazo
+		var dir = transform.position - Cursor.transform.position;
+ 		var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+ 		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+		//Para ver si el brazo se encuetra detras o delante del jugador
+
+		if(animator.GetBool("isRight")){
+
+			GetComponent<SpriteRenderer>().sortingOrder=3;
+
+		}else{
+
+			GetComponent<SpriteRenderer>().sortingOrder=1;
+		}
+
+		// Comprueba si hace falta hacer flip al personaje dependiendo de dónde esté apuntando
+		if(angle > -90 && angle < 90) {
+			animator.SetBool("isRight", false);
+		} else {
+			animator.SetBool("isRight", true);
+		}
+
+	}
+}
---
+++ Terminal Drive/Assets/Resources/Player/Control/PointerControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerControl : MonoBehaviour {
+
+	Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+		mainCamera = Camera.main;
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//Codigo para mover el cursor
+		Vector3 pointer = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+		gameObject.transform.position = new Vector3(pointer.x, pointer.y, 0);
+
+
+    }
+
+		
+		
+	
+}
