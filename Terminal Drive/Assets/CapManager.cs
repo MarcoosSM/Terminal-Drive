166ea3164865025a5393de610d514e5de4e274ed
Alexander <40186295+malyga@users.noreply.github.com>
2018-11-06 11:04:26 +0100

    Añadido manager de casquillos.
    Ahora se eliminan los casquillos tras un tiempo. (Igual que los proyectiles)

---
+++ Terminal Drive/Assets/CapManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapManager : MonoBehaviour {
+
+	public int maxCapTime = 3;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(DestroyTimeOut());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator DestroyTimeOut(){
+		
+		yield return new WaitForSeconds(maxCapTime);
+
+		Destroy(gameObject);
+ 	}
+}
