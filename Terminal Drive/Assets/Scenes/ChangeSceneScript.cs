413551f9bcc6255d897255c0532e21006eec51ed
MiguelAngelDAM <44416407+MiguelAngelDAM@users.noreply.github.com>
2018-11-13 16:48:32 +0100

    Scena de menu mejorada ( Con cambio de escena )

---
+++ Terminal Drive/Assets/Scenes/ChangeSceneScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeSceneScript : MonoBehaviour {
+	[SerializeField] private Object choosenScene;
+	[SerializeField] private Button clickedButton;
+
+	public void ChangeScene() {
+		SceneManager.LoadScene(choosenScene.name);
+		Debug.Log("Scene loaded");
+	}
+}
