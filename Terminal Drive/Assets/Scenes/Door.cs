d58357c5d5c05b06abf73c5cf7bf82c0753c00b3
fantomas08 <44437583+fantomas08@users.noreply.github.com>
2018-11-23 19:14:55 +0100

    dasd

---
+++ Terminal Drive/Assets/Scenes/Door.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+	[SerializeField] Sprite sp;
+	SpriteRenderer sr;
+	// Use this for initialization
+	void Start () {
+		sr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.gameObject.tag == ("Player")){
+			sr.sprite = sp;
+		}
+	}
+}
