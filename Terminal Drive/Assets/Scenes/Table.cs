77948facaa6a431831a4c1f44d3506a654701b2a
fantomas08 <44437583+fantomas08@users.noreply.github.com>
2018-11-27 17:06:37 +0100

    scene changes and add table

---
+++ Terminal Drive/Assets/Scenes/Table.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Table : MonoBehaviour {
+
+	[SerializeField] Collider2D collider;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D(Collider2D other){
+		if (other.gameObject.tag == "Player" && Input.GetKeyDown("e")){
+			collider.enabled = true;
+			//transform.Rotate(0,90,0);
+		}		
+	}
+}
