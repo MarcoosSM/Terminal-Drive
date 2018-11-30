15888711edb6d009836e36762fabad8c8e2e0c8f
fantomas08 <44437583+fantomas08@users.noreply.github.com>
2018-11-30 19:12:29 +0100

    Table funtion

--- Terminal Drive/Assets/Scenes/Table.cs
+++ Terminal Drive/Assets/Scenes/Table.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Table : MonoBehaviour {
 
 	[SerializeField] Collider2D collider;
+	Animator animator;
+	GameObject player;
 	// Use this for initialization
 	void Start () {
-		
+		player = GameObject.FindGameObjectWithTag("Player");
+		animator = player.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -18,7 +21,13 @@ public class Table : MonoBehaviour {
 	void OnTriggerStay2D(Collider2D other){
 		if (other.gameObject.tag == "Player" && Input.GetKeyDown("e")){
 			collider.enabled = true;
-			//transform.Rotate(0,90,0);
+			GameObject player = other.gameObject;
+			
+			if (animator.GetBool("isRight")){
+				transform.localEulerAngles = new Vector3(0,0,-90);
+			} else {
+				transform.localEulerAngles = new Vector3(0,0,90);
+			}
 		}		
 	}
 }
