941c3fe3b7a5c3c83528d66d8720ab51dee44889
Nismal <ismaelperez1299@gmail.com>
2018-12-03 17:06:57 +0100

    Añadido MedPack

---
+++ Terminal Drive/Assets/Resources/Combat System/HealPack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealPack : MonoBehaviour {
+
+	AudioSource audioSource;
+
+	void Awake() {
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		if (Input.GetButtonDown("Use") && other.gameObject.tag == "Player") {
+			InventoryControl IC = other.GetComponent<InventoryControl>();
+			IC.Health = IC.MaxHealth;
+				if(!audioSource.isPlaying){
+				audioSource.Play();
+			}
+		}
+	}
+}
