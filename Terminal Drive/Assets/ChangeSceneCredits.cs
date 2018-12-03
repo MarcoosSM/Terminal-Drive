34e0a3e727fadf36e4cedc3262f9474223e065bb
malyga <40186295+malyga@users.noreply.github.com>
2018-12-03 17:32:49 +0100

    La última puerta da a los créditos

---
+++ Terminal Drive/Assets/ChangeSceneCredits.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ChangeSceneCredits : MonoBehaviour {
+
+	[SerializeField] private Object choosenScene;
+	[SerializeField] Sprite open;
+	[SerializeField] Sprite close;
+	SpriteRenderer sr;
+	AudioSource audioSource;
+	
+	void Start() {
+		sr = GetComponent<SpriteRenderer>();
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		if (Input.GetButtonDown("Use") && other.gameObject.tag == "Player") {
+			sr.sprite = open;
+			audioSource.Play();
+			StartCoroutine(pause(other));
+		}
+	}
+
+	IEnumerator pause(Collider2D other) {
+		yield return new WaitForSeconds(1f);
+		sr.sprite = close;
+		ChangeScene();
+	}
+
+	public void ChangeScene() {
+		SceneManager.LoadScene(choosenScene.name);
+	}
+
+	
+}
