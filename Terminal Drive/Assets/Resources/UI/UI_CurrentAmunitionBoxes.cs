9b42723c9a6e63cdd04b36b76e44566c945f10a2
Nismal <ismaelperez1299@gmail.com>
2018-12-09 17:22:49 +0100

    Añadidos numero de balas en reserva a la HUD

---
+++ Terminal Drive/Assets/Resources/UI/UI_CurrentAmunitionBoxes.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_CurrentAmunitionBoxes : MonoBehaviour {
+
+	
+	[SerializeField]Text box1; 
+	[SerializeField]Text box2; 
+	[SerializeField]Text box3; 
+
+	WeaponController Weapon1;
+	WeaponController Weapon2;
+	WeaponController Weapon3;
+
+	InventoryControl InventoryControl;
+	void Start(){
+		InventoryControl =  GameObject.FindGameObjectWithTag("Player").GetComponent<InventoryControl>();
+		Weapon1 = InventoryControl.guns[0].GetComponent<WeaponController>();
+		Weapon2 = InventoryControl.guns[1].GetComponent<WeaponController>();
+		Weapon3 = InventoryControl.guns[2].GetComponent<WeaponController>();
+
+	}
+	void Update () {
+		box1.text = (Weapon1.reserveAmmo).ToString();
+		box2.text = (Weapon2.reserveAmmo).ToString();
+		box3.text = (Weapon3.reserveAmmo).ToString();
+
+	}
+}
